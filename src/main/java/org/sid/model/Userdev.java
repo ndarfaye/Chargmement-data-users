ac09abb09f91e56f6bd58f6a9508c06b11bbf428
Faye Ndar <ndarfaye97@gmail.com>
2023-05-31 00:25:29 +0100

    seconde commit

---
+++ src/main/java/org/sid/model/Userdev.java
@@ -0,0 +1,20 @@
+package org.sid.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Data
+public class User {
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String email;
+    private int age;
+}
